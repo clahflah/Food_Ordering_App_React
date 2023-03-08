4e9ca5abf097d9b787b250af3cf7536fcccc8595
clahflah <claytonflaherty92@gmail.com>
2023-03-08 05:35:23 -0500

    fixed bug

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,7 +1,7 @@
 import React from "react";
 import mealsImage from "../../assets/meals.jpeg"
 
-import classes from "./Header.modules.css"
+import classes from "./Header.module.css"
 
 const Header = props => {
   return (
