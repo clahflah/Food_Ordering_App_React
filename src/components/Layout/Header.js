75b9e1dfa905f90ef95679edc0db7a63d895f706
clahflah <claytonflaherty92@gmail.com>
2023-03-08 05:32:31 -0500

    header created

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,1 +1,20 @@
 import React from "react";
+import mealsImage from "../../assets/meals.jpeg"
+
+import classes from "./Header.modules.css"
+
+const Header = props => {
+  return (
+  <React.Fragment>
+    <header className={classes.header}>
+      <h1>MealzOnReact</h1>
+      <button>Cart</button>
+    </header>
+    <div className={classes['main-image']}>
+      <img src={mealsImage} alt="Tasty meals"/>
+    </div>
+  </React.Fragment>
+  )
+};
+
+export default Header;
