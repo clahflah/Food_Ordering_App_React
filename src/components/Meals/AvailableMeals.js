fbb35179c5e62419cdf3f694a22f41252171e577
clahflah <claytonflaherty92@gmail.com>
2023-03-09 05:24:46 -0500

    woo

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,4 +1,5 @@
 import MealItem from "./MealItem";
+import Card from "../UI/Card";
 
 import classes from "./AvailableMeals.module.css";
 
@@ -41,7 +42,9 @@ const AvailableMeals = () => {
   ));
   return (
     <section className={classes.meals}>
-      <ul>{mealsList}</ul>
+      <Card>
+        <ul>{mealsList}</ul>
+      </Card>
     </section>
   );
 };
--- src/components/Meals/MealItem.js
+++ src/components/Meals/MealItem.js
@@ -1,16 +1,15 @@
 import React from "react";
 
-import classes from './MealItem.module.css'
+import classes from "./MealItem.module.css";
 
-const MealItem = props => {
-
+const MealItem = (props) => {
   return (
     <div className={classes.name}>
       <h3>{props.name}</h3>
       <h2 className={classes.description}>{props.description}</h2>
       <h2 className={classes.price}>{props.price}</h2>
     </div>
-  )
+  );
 };
 
-export default MealItem
+export default MealItem;
