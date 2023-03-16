dba485f9b3f2267fbeb5256097b097685d7ea093
clahflah <claytonflaherty92@gmail.com>
2023-03-16 05:34:53 -0400

    cart functional

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -9,20 +9,25 @@ import CartItem from "./CartItem";
 const Cart = (props) => {
   const cartCtx = useContext(CartContext);
 
-  const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`
-  const hasItems = cartCtx.items.length > 0
+  const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
+  const hasItems = cartCtx.items.length > 0;
 
-  const cartItemRemoveHandler = id = {};
+  const cartItemRemoveHandler = id => {};
 
-  const cartItemAddHandler = item - {};
+  const cartItemAddHandler = item => {};
 
   const cartItems = (
     <ul className={classes["cart-item"]}>
-      {cartCtx.items.map(
-        (item) => (
-          <CartItem key={item.id} name={item.name} amount={item.amount} price={item.price} onRemove={cartItemRemoveHandler} onAdd={cartItemAddHandler}/>
-        )
-      )}
+      {cartCtx.items.map((item) => (
+        <CartItem
+          key={item.id}
+          name={item.name}
+          amount={item.amount}
+          price={item.price}
+          onRemove={cartItemRemoveHandler.bind(null,item.id)}
+          onAdd={cartItemAddHandler.bind(null,item)}
+        />
+      ))}
     </ul>
   );
 
